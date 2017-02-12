f268f10bd0f844c7a765e50a35b70fb153cb12a7
Igor <igozzz94@gmail.com>
2017-02-12 15:28:37 +0300

    Controller function to load and save color pallet. Sample palette in resources directory.

--- src/View/SuperPixelEditorView.java
+++ src/View/SuperPixelEditorView.java
@@ -5,19 +5,24 @@
  */
 package View;
 
+import Controller.ColorPaletteController;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author Andrew
@@ -37,6 +42,8 @@ public class SuperPixelEditorView extends Application {
     colorPickerStage.setTitle("Color Picker");
     colorPickerStage.setScene(colorPickerScene);
 
+    HBox hbButtons = new HBox();
+
     Button btn = new Button();
     btn.setText("Color Picker");
     btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -46,10 +53,33 @@ public class SuperPixelEditorView extends Application {
       }
     });
 
-    StackPane root = new StackPane();
-    root.getChildren().add(btn);
+    Button saveBtn = new Button();
+    saveBtn.setText("Save Custom Colors");
+    saveBtn.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        ColorPaletteController.saveColorPalette(colorPicker);
+      }
 
-    Scene scene = new Scene(root, 300, 250);
+    });
+
+    Button loadBtn = new Button();
+    loadBtn.setText("Load Custom Colors");
+    loadBtn.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        ColorPaletteController.loadColorPalette(colorPicker);
+      }
+
+    });
+
+    StackPane root = new StackPane();
+    hbButtons.getChildren().add(btn);
+    hbButtons.getChildren().add(saveBtn);
+    hbButtons.getChildren().add(loadBtn);
+    root.getChildren().add(hbButtons);
+
+    Scene scene = new Scene(root, 400, 250);
 
     primaryStage.setTitle("Super Pixel Art Editor");
     primaryStage.setScene(scene);
